c2389b590ae29e4da2777432770cce0ae3d760cb
StevenRSS <ssunarto@student.ciputra.ac.id>
2023-04-27 20:37:12 +0700

    Initial Commit

---
+++ Landmarks-AFL3/Landmarks_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_AFL3App.swift
+//  Landmarks-AFL3
+//
+//  Created by MacBook Pro on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
