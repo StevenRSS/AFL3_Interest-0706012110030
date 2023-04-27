5f2bf20e09f9040e9694f2bc99b1a8f12a4ac74d
StevenRSS <ssunarto@student.ciputra.ac.id>
2023-04-27 21:19:58 +0700

    Chapter 2 Section 4

---
+++ Landmarks-AFL3/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks-AFL3
+//
+//  Created by MacBook Pro on 27/04/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
