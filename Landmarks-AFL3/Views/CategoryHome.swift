9fc9a942952e8300f0926e5b4b3f877b5541730c
StevenRSS <ssunarto@student.ciputra.ac.id>
2023-04-28 06:12:02 +0700

    Chapter 6 Section 1

---
+++ Landmarks-AFL3/Views/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks-AFL3
+//
+//  Created by MacBook Pro on 28/04/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
