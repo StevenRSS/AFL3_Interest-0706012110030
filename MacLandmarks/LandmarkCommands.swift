0527d729e8ae549cf6eb28d89ce7d879d81f0d3d
StevenRSS <ssunarto@student.ciputra.ac.id>
2023-04-28 09:30:00 +0700

    Chapter 10 Section 5

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks-AFL3
+//
+//  Created by MacBook Pro on 28/04/23.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
